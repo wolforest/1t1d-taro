019c8f9fc2d39bf326382e10e685ca8e9402bdec
tiejia <niexq0319@gmail.com>
2024-12-10 10:03:46 +0800

    feat: Taro路由之switchTab

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,12 +2,38 @@ export default defineAppConfig({
   pages: [
     'pages/splash/index',
     'pages/intro/index',
-    'pages/index/index'
+    'pages/index/index',
+    'pages/favorites/index',
+    'pages/mine/index',
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    selectedColor: '#d55f5b',
+    backgroundColor: '#ffffff',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '',
+        iconPath: 'assets/home_icon.png',
+        selectedIconPath: 'assets/home_icon_active.png',
+      },
+      {
+        pagePath: 'pages/favorites/index',
+        text: '',
+        iconPath: 'assets/favorites_icon.png',
+        selectedIconPath: 'assets/favorites_icon_active.png',
+      },
+      {
+        pagePath: 'pages/mine/index',
+        text: '',
+        iconPath: 'assets/mine_icon.png',
+        selectedIconPath: 'assets/mine_icon_active.png',
+      },
+    ],
+  },
 })
---
+++ src/pages/favorites/index.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import { View } from '@tarojs/components';
+import styles from './index.module.scss'
+
+const Index = () => {
+  return (
+    <View className={styles.favorites}>
+      收藏页面
+    </View>
+  )
+}
+
+export default Index
